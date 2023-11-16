5fa48a8228a8e27fe975c830fe06048e51b2429e
Pavel Braginskiy <pavelbraginskiy@hotmail.com>
2023-11-15 20:10:30 -0800

    Run all algorithms in one go

--- compare.py
+++ compare.py
@@ -19,4 +19,13 @@ if __name__ == '__main__':
     a_sample = a[len(a) // 2 - s:len(a) // 2 + s]
     b_sample = b[len(b) // 2 - s:len(b) // 2 + s]
 
-    print(similarity.erp(a_sample, b_sample))
+    print("CPD: ", similarity.cpd(a_sample, b_sample))
+    print("SPD: ", similarity.spd(a_sample, b_sample))
+    print("DTW: ", similarity.cpd(a_sample, b_sample))
+    print("LCSS, e=0.5: ", similarity.lcss(a_sample, b_sample, 0.5))
+    print("LCSS, e=1: ", similarity.lcss(a_sample, b_sample, 1))
+    print("LCSS, e=2: ", similarity.lcss(a_sample, b_sample, 2))
+    print("EDR, e=0.5: ", similarity.edr(a_sample, b_sample, 0.5))
+    print("EDR, e=1: ", similarity.edr(a_sample, b_sample, 1))
+    print("EDR, e=2: ", similarity.edr(a_sample, b_sample, 2))
+    print("ERP: ", similarity.erp(a_sample, b_sample))
